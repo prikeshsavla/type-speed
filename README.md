f2522311d2b64ddd5cb49750af6cb1849c45d4da
Prikesh Savla <prikesh@cashee.com>
2024-10-12 13:09:24 +0530

    Add Base feature set without timers

---
+++ README.md
@@ -0,0 +1,13 @@
+### Objective
+
+Typing speed tester 
+
+### Features
+- [] Generate words
+- [] Take input of words
+- [] visually validate input matches
+- [] time inputs to 180s
+- [] If a sentence is completed show a new sentence
+- [] Track total valid characters entered + total words input (floor for time)
+- [] Flavour the generated words on theme
+
---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker';
+import './App.css'
+
+function App() {
+  const [phrase, setPhrase] = useState(faker.company.catchPhrase().toLowerCase())
+  const [postContent, setPostContent] = useState('');
+  const [validInput, setValidInput] = useState('');
+  const [finalInput, setFinalInput] = useState('');
+  // const [backgroundColor, setBackgroundColor] = useState('lightgreen');
+
+  useEffect(() => {
+    if(compareString(phrase, postContent)){
+      setValidInput(postContent)
+    }
+    if(phrase == postContent){
+      setFinalInput(finalInput + validInput)
+      setPhrase(faker.company.catchPhrase().toLowerCase())
+      setPostContent('')
+    }
+  }, [finalInput, validInput, phrase, postContent]);
+
+  const validatePostContent =  (): string => {
+    if(compareString(phrase, postContent)){
+      return 'lightgreen'
+    }
+    else {
+      return 'orange'
+    }
+  }
+
+  const compareString = (original: string, input:string) : boolean => {
+    return original.indexOf(input) == 0 
+  }
+  
+  return (
+    <>
+      <p>
+      {phrase}
+      </p>
+
+      <textarea style={{backgroundColor: validatePostContent(), color: 'black'}} cols={70} rows={20} value={postContent} 
+      onChange={e => setPostContent(e.target.value)} ></textarea>
+      <div>
+        <ul>
+          <li>Char count: {(finalInput + validInput).length} / {(finalInput + phrase).length}</li>
+          <li>Word count: {(finalInput + validInput).split(' ').length} / {(finalInput +phrase).split(" ").length}</li>
+          <li>Time Left: 10s</li>
+        </ul>
+        
+        <p>
+        ValidInput: {validInput}
+        </p>
+        <p>
+          Final Input: {finalInput}
+        </p>
+        </div>
+    </>
+  )
+}
+
+export default App
